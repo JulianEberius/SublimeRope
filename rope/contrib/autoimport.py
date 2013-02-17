cd81962493320b031141206d5dcdb17fa0bcddf5
Julian Eberius <ebi@twinguin.local>
2013-02-17 12:55:57 +0100

    - fixed re.M bug in Rope's autoimport (should improve performance)

--- rope/contrib/autoimport.py
+++ rope/contrib/autoimport.py
@@ -126,7 +126,7 @@ class AutoImport(object):
 
     def find_insertion_line(self, code):
         """Guess at what line the new import should be inserted"""
-        match = re.search(r'^(def|class)\s+', code)
+        match = re.search(r'^(def|class)\s+', code, re.M)
         if match is not None:
             code = code[:match.start()]
         try:
